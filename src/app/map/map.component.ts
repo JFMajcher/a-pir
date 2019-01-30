14bf941063570af3afc42095ddd7f6aca923d8aa
Mateusz Wojewódka <matw@netcompany.com>
2019-01-30 14:05:41 +0100

    Dziala wczytywanie dwoch ikonek

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, NgZone, OnInit, AfterContentInit, ContentChildren, Directive, EventEmitter, OnChanges, OnDestroy, Output, QueryList, SimpleChange, forwardRef} from '@angular/core';
+import { Component, Input, ViewChild, NgZone, OnInit, AfterContentInit, ContentChildren, Directive, EventEmitter, OnChanges, OnDestroy, Output, QueryList, SimpleChange, forwardRef } from '@angular/core';
 import { MapsAPILoader, AgmMap } from '@agm/core';
 import { GoogleMapsAPIWrapper } from '@agm/core/services';
 import { Observable, ReplaySubject, Subscription } from 'rxjs';
@@ -9,19 +9,13 @@ import * as mapTypes from '@agm/core/services/google-maps-types';
 import { MarkerManager } from '@agm/core/services/managers/marker-manager';
 import { AgmInfoWindow } from '@agm/core/directives/info-window';
 
-
-
 declare var google: any;
 
 interface Marker {
   lat: number;
   lng: number;
   value: number;
-  iconUrl1: string;
-  iconUrl2: string;
-  iconUrl3: string;
-  iconUrl4: string;
-  iconUrlx: string;
+  iconUrl: string;
 }
 
 interface Location {
@@ -29,33 +23,46 @@ interface Location {
   lng: number;
   viewport?: Object;
   zoom: number;
-  marker?: Marker;
 }
+
+const greenIcon: string = "http://labs.google.com/ridefinder/images/mm_20_green.png ";
+const yellowIcon: string = "http://labs.google.com/ridefinder/images/mm_20_yellow.png ";
+const orangeIcon: string = "http://labs.google.com/ridefinder/images/mm_20_orange.png ";
+const redIcon: string = "http://labs.google.com/ridefinder/images/mm_20_red.png ";
+// const iconUrlx: string = " ";
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
 })
 export class MapComponent implements OnInit {
   geocoder: any;
-  public location: Location = {
+
+  public mapStartup: Location = {
     lat: 52.202737,
     lng: 21.001095,
-    marker: {
-      lat: 52.202737,
-      lng: 21.001095,
-      value: 11,
-      iconUrl1: "http://labs.google.com/ridefinder/images/mm_20_green.png ",
-      iconUrl2: "http://labs.google.com/ridefinder/images/mm_20_yellow.png ",
-      iconUrl3: "http://labs.google.com/ridefinder/images/mm_20_orange.png ",
-      iconUrl4: "http://labs.google.com/ridefinder/images/mm_20_red.png ",
-      iconUrlx: " "
-    },
     zoom: 13
+  }
+
+  public location: Marker = {
+    lat: 52.202737,
+    lng: 21.001095,
+    value: 11,
+    iconUrl: greenIcon
   };
 
+  public location2: Marker = {
+    lat: 52.209999,
+    lng: 21.009999,
+    value: 40,
+    iconUrl: orangeIcon
+  };
 
+  public locations: Marker[] = [
+    this.location,
+    this.location2
+  ];
 
   @ViewChild(AgmMap) map: AgmMap;
 
@@ -72,6 +79,6 @@ export class MapComponent implements OnInit {
 
 
   ngOnInit() {
-    
+
   }
 }
